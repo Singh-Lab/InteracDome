8a1bc51417c383bcb6d0de5c3a78a323b3ce65a9
Shilpa N. Kobren <snadimpa@princeton.edu>
2017-07-04 22:54:07 -0400

    fixed typo in argument parser for available options

--- create_fasta.py
+++ create_fasta.py
@@ -35,12 +35,104 @@ def mean(numbers):
 
 ########################################################################################################
 
-def create_biolip_fasta_files(distance_file, fasta_file, current_pdb_id, distance_metric):
+def receptor_ligand_distance(euc_dist, vdw_dist, vdw_error, std_dist, std_error, distance):
+  """
+  :param euc_dist: Euclidean distance between two atoms specified by their (x, y, z) coordinates
+  :param vdw_dist: overlap area between two Gaussians centered at teh two (x, y, z) atom locations, with 
+                   standard deviations set the respective van der Waals' interaction radii of the two atoms
+  :param vdw_error: computation error from calculating the overlap area (integral from -Inf to Inf)
+  :param std_dist: as in vdw_dist, but where the standard deviations of the Gaussians are set to 1.5
+  :param std_error: as in vdw_error, except between two Gaussians with standard deviations set to 1.5
+  :param distance: metric used to determine the distance between the two atoms
+  :return: the "distance" between the two atoms according to the specified distance metric
+  """
+
+  # Euclidean distance to a particular ligand atom
+  if distance in ['mindist', 'meandist', 'fracin4']:
+    current_distance = float(euc_dist)
+
+  # Overlap area (integrated) between two normal distributions with 1.5 standard deviation across the board
+  elif distance in ['maxstd', 'meanstd', 'sumstd']:
+    if std_dist and std_dist != 'N/A' and std_error and float(std_error) < float(std_dist):
+      current_distance = float(std_dist)
+    else:
+      current_distance = 'N/A'
+
+  # Overlap area (integrated) between two normal distributions with vdw interaction radii as standard deviation
+  elif distance in ['maxvdw', 'maxstd', 'sumvdw']:
+    if vdw_dist and vdw_dist != 'N/A' and vdw_error and float(vdw_error) < float(vdw_dist):
+      current_distance = float(vdw_dist)
+    else:
+      current_distance = 'N/A'
+
+  else:
+    current_distance = 'N/A'
+
+  return current_distance
+
+
+########################################################################################################
+
+def residue_binding_score(residue, ligand_type, distance):
+  """
+  :param residue: list of tuples (atom ID, ligand atom type, distance)
+  :param ligand_type: type of ligand that we are currently considering
+  :param distance: scoring metric for condensing information about all heavy side-chain residue atoms 
+                   into a single score
+  :return: score for the residue according to the specified distance metric
+  """
+
+  # final score for this particular residue_aa_index to the current ligand type
+  residue_score = 0. if distance != 'mindist' else DISTANCE_CUTOFF
+
+  if distance == 'fracin4':  # FRACTION of atoms that are within 4AA of THIS ligand.
+    total_atoms = [float(res_atom_id.split('/')[1])
+                   for (res_atom_id, lig_id, _) in residue if lig_id == ligand_type][0]
+    residue_score = len(set([res_atom_id for (res_atom_id, lig_id, curr_dist) in residue
+                             if lig_id == ligand_type and float(curr_dist) <= 4])) / total_atoms
+
+  elif distance == 'mindist':  # MINIMUM distance of ANY atom to this ligand...
+    residue_score = min([curr_dist for (_, lig_id, curr_dist) in residue if lig_id == ligand_type])
+
+  elif distance == 'meandist':  # AVERAGE distance of ANY atom to a ligand.
+    scores = []
+    for residue_atom_id in set([res_atom_id for (res_atom_id, _, _) in residue]):
+      current_score = [curr_dist for (res_atom_id, lig_id, curr_dist) in residue
+                       if lig_id == ligand_type and res_atom_id == residue_atom_id]
+      scores.append(min(current_score) if len(current_score) > 0 else DISTANCE_CUTOFF)
+    residue_score = mean(scores)
+
+  elif distance in ['maxstd', 'maxvdw']:  # MAXIMUM "closeness" score of ANY atom
+    residue_score = 0.
+    for residue_atom_id in set([res_atom_id for (res_atom_id, _, _) in residue]):
+      current_score = sum([curr_dist for (res_atom_id, lig_id, curr_dist) in residue
+                           if lig_id == ligand_type and res_atom_id == residue_atom_id])
+      if current_score > residue_score:
+        residue_score = current_score
+
+  elif distance in ['meanstd', 'meanvdw']:  # AVERAGE "closeness" score across all atoms
+    scores = []
+    for residue_atom_id in set([res_atom_id for (res_atom_id, _, _) in residue]):
+      current_score = sum([curr_dist for (res_atom_id, lig_id, curr_dist) in residue
+                           if lig_id == ligand_type and res_atom_id == residue_atom_id])
+      scores.append(current_score)
+    residue_score = mean(scores)
+
+  elif distance in ['sumstd', 'sumvdw']:  # SUM of "closeness" scores of all atoms!
+    # Note: sum of an empty list returns 0
+    residue_score = sum([curr_dist for (_, lig_id, curr_dist) in residue if lig_id == ligand_type])
+
+  return residue_score
+
+
+########################################################################################################
+
+def create_biolip_fasta_files(distance_file, fasta_file, current_pdb_id, distance):
   """
   :param distance_file: full path to a "_distances.txt.gz" file as created by calculate_distances.py
   :param fasta_file: full path to an output "_distancetype.fa" where output should be written to
   :param current_pdb_id: PDB ID that the distance file should correspond to
-  :param distance_metric: type of per-residue score measuring the binding status of an amino acid residue.
+  :param distance: type of per-residue score measuring the binding status of an amino acid residue.
                           (e.g., mindist, meandist, fracin4, maxstd, meanstd, sumstd, maxvdw, meanvdw, sumvdw)
   :return: full path to a recently created fasta file where the sequence ID header line contains binding info
   """
@@ -86,25 +178,7 @@ def create_biolip_fasta_files(distance_file, fasta_file, current_pdb_id, distanc
       binding_residues[pdb_chain_id][int(residue_aa_index)] = []
 
     # set the current_distance accordingly:
-    current_distance = None
-
-    # Euclidean distance to a particular ligand atom
-    if distance_metric in ['mindist', 'meandist', 'fracin4']:
-      current_distance = float(euc_dist)
-
-    # Overlap area (integrated) between two normal distributions with 1.5 standard deviation across the board
-    elif distance_metric in ['maxstd', 'meanstd', 'sumstd']:
-      if std_dist and std_dist != 'N/A' and std_error and float(std_error) < float(std_dist):
-        current_distance = float(std_dist)
-      else:
-        current_distance = 'N/A'
-
-    # Overlap area (integrated) between two normal distributions with vdw interaction radii as standard deviation
-    elif distance_metric in ['maxvdw', 'maxstd', 'sumvdw']:
-      if vdw_dist and vdw_dist != 'N/A' and vdw_error and float(vdw_error) < float(vdw_dist):
-        current_distance = float(vdw_dist)
-      else:
-        current_distance = 'N/A'
+    current_distance = receptor_ligand_distance(euc_dist, vdw_dist, vdw_error, std_dist, std_error, distance)
 
     try:
       new_atom_info = (residue_atom_id,
@@ -113,6 +187,7 @@ def create_biolip_fasta_files(distance_file, fasta_file, current_pdb_id, distanc
       binding_residues[pdb_chain_id][int(residue_aa_index)].append(new_atom_info)
     except ValueError:  # if distance cannot be converted to a float, then we do not keep track of it
       continue
+
   distance_handle.close()
 
   # create the fasta file as specified:
@@ -125,41 +200,12 @@ def create_biolip_fasta_files(distance_file, fasta_file, current_pdb_id, distanc
     fasta_outhandle.write('>' + pdb_chain_id + ' bindingSiteRes=')
 
     final_binding_residues = []
-    for residue_aa_index, current_binding_residues in binding_residues[pdb_chain_id].items():
-      for ligand_id in set([lig_id for (_, lig_id, _) in current_binding_residues]):
+    for residue_aa_index, current_binding_residue in binding_residues[pdb_chain_id].items():
+      for ligand_id in set([lig_id for (_, lig_id, _) in current_binding_residue]):
 
-        v = None  # final score for this particular residue_aa_index to the current ligand type
+        current_residue_score = residue_binding_score(current_binding_residue, ligand_id, distance)
 
-        if distance_metric == 'fracin4':  # FRACTION of atoms that are within 4AA of THIS ligand.
-          total_atoms = [float(res_atom_id.split('/')[1])
-                         for (res_atom_id, lig_id, _) in current_binding_residues if lig_id == ligand_id][0]
-          v = len(set([res_atom_id for (res_atom_id, lig_id, curr_dist) in current_binding_residues
-                       if lig_id == ligand_id and float(curr_dist) <= 4])) / total_atoms
-
-        elif distance_metric == 'mindist':  # MINIMUM distance of ANY atom to this ligand...
-          v = min([curr_dist for (_, lig_id, curr_dist) in current_binding_residues if lig_id == ligand_id])
-
-        elif distance_metric == 'meandist':  # AVERAGE distance of ANY atom to a ligand.
-          scores = []
-          for residue_atom_id in set([res_atom_id for (res_atom_id, _, _) in current_binding_residues]):
-            current_score = [curr_dist for (res_atom_id, lig_id, curr_dist) in current_binding_residues
-                             if lig_id == ligand_id and res_atom_id == residue_atom_id]
-            scores.append(min(current_score) if len(current_score) > 0 else DISTANCE_CUTOFF)
-          v = mean(scores)
-
-        elif distance_metric in ['maxstd', 'maxvdw']:  # MAXIMUM "closeness" score of ANY atom
-          v = 0
-          for residue_atom_id in set([res_atom_id for (res_atom_id, _, _) in current_binding_residues]):
-            current_score = sum([curr_dist for (res_atom_id, lig_id, curr_dist) in current_binding_residues
-                                 if lig_id == ligand_id and res_atom_id == residue_atom_id])
-            if current_score > v:
-              v = current_score
-
-        elif distance_metric in ['sumstd', 'sumvdw']:  # SUM of "closeness" scores of all atoms!
-          # Note: sum of an empty list returns 0
-          v = sum([curr_dist for (_, lig_id, curr_dist) in current_binding_residues if lig_id == ligand_id])
-
-        final_binding_residues.append((residue_aa_index, ligand_id, v))
+        final_binding_residues.append((residue_aa_index, ligand_id, current_residue_score))
     fasta_outhandle.write(','.join([str(res_aa_index) + '-' + lig_id + '-' + str(final_score)
                                     for (res_aa_index, lig_id, final_score) in sorted(final_binding_residues)]) +
                           ';\n' + receptor_chain_sequences[pdb_chain_id] + '\n\n')
@@ -179,11 +225,11 @@ if __name__ == "__main__":
   parser.add_argument('--prefix', type=str,
                       help='Two letter prefix to subset of PDB IDs to process',
                       default='10')
-  parser.add_argument('--distance_metric', type=str,
+  parser.add_argument('--distance', type=str,
                       help='How to record the distance between receptor and ligand?',
                       default='mindist',
                       choices={'mindist', 'fracin4', 'meandist', 'maxstd', 'meanstd', 'sumstd',
-                               'maxvdw', 'meanvdw', 'sumstd'})
+                               'maxvdw', 'meanvdw', 'sumvdw'})
 
   args = parser.parse_args()
 
@@ -231,5 +277,5 @@ if __name__ == "__main__":
 
   for infile in distlist_files:
     distance_infile = distance_out_directory + infile
-    fasta_outfile = fasta_out_directory+infile.replace('_distances.txt', '_'+args.distance_metric+'.fa')
-    create_biolip_fasta_files(distance_infile, fasta_outfile, infile[27:31], args.distance_metric)
+    fasta_outfile = fasta_out_directory+infile.replace('_distances.txt', '_'+args.distance+'.fa')
+    create_biolip_fasta_files(distance_infile, fasta_outfile, infile[27:31], args.distance)
